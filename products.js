bcb4ddcc24272321359b990c4f11e4b59ba860ce
joaopedronm <joaopedron253@gmail.com>
2023-05-24 15:57:19 -0300

    Criação da página single-product de forma dinâmica com JavaScript

--- products.js
+++ products.js
@@ -1,88 +1,19 @@
-// IMAGENS DOS PRODUTOS
-
-const images = []
-
-const arrImg1 = []
-
-let img1 = new Image()
-img1.src = "./img/jerseys/gallery/mahomes-01.png"
-const img2 = new Image()
-img2.src = "./img/jerseys/gallery/mahomes-02.png"
-const img3 = new Image()
-img3.src = "./img/jerseys/gallery/mahomes-03.png"
-
-arrImg1.push(img1, img2, img3)
-
-const arrImg2 = []
-
-const img4 = new Image()
-img4.src = "./img/jerseys/gallery/josh-allen-01.png"
-const img5 = new Image()
-img5.src = "./img/jerseys/gallery/josh-allen-02.png"
-const img6 = new Image()
-img6.src = "./img/jerseys/gallery/josh-allen-03.png"
-
-arrImg2.push(img4, img5, img6)
-
-const arrImg3 = []
-
-const img7 = new Image()
-img7.src = "./img/jerseys/gallery/joe-burrow-01.png"
-const img8 = new Image()
-img8.src = "./img/jerseys/gallery/joe-burrow-02.png"
-const img9 = new Image()
-img9.src = "./img/jerseys/gallery/joe-burrow-03.png"
-
-arrImg3.push(img7, img8, img9)
-
-// Inserindo os arrays de imagens dentro do array principal das imagens:
-
-images.push(arrImg1, arrImg2, arrImg3)
-
-
-
-// const products = [
-//     {
-//         id: 1,
-//         title: 'Patrick Mahomes - Chiefs',
-//         price: 219.90,
-//         img1: images[0][0],
-//         img2: images[0][1],
-//         img3: images[0][2]
-//     },
-//     {
-//         id: 2,
-//         title: 'Josh Allen - Bills',
-//         price: 219.90,
-//         img1: images[1][0],
-//         img2: images[1][1],
-//         img3: images[1][2]
-//     },
-//     {
-//         id: 3,
-//         title: 'Joe Burrow - Bengals',
-//         price: 199.90,
-//         img1: images[2][0],
-//         img2: images[2][1],
-//         img3: images[2][2]
-//     }
-// ]
-
 let url_atual = window.location.href;
 
-const products = document.getElementsByClassName("single-product");
+// Selecionar todos os elementos com a classe "single-product"
+const products = document.querySelectorAll('.single-product');
 
-for (let i = 0; i < products.length; i++) {
-    products[i].addEventListener("click", function() {
-      // Obtém as informações do produto clicado
-      const span = this.querySelector(".description span");
-      const h5 = this.querySelector(".description h5");
-      const h4 = this.querySelector(".description h4");
-  
-      // Cria uma URL dinâmica para a página do produto
-      const productURL = "product.html?" + encodeURIComponent(h5.textContent);
-  
-      // Redireciona para a página do produto
-      window.location.href = productURL;
-    });
-}
+// Adicionar um evento de clique a cada produto
+products.forEach(product => {
+  product.addEventListener('click', () => {
+    // Extrair as informações do produto
+    const title = product.querySelector('h5').textContent;
+    const price = product.querySelector('h4').textContent;
+    const image1Src = product.querySelector('#img1').getAttribute('src');
+    const image2Src = product.querySelector('#img2').getAttribute('src');
+    const image3Src = product.querySelector('#img3').getAttribute('src');
+
+    // Redirecionar para a página do produto com as informações
+    window.location.href = `single-product.html?title=${encodeURIComponent(title)}&price=${encodeURIComponent(price)}&image=${encodeURIComponent(image1Src)}&image2=${encodeURIComponent(image2Src)}&image3=${encodeURIComponent(image3Src)}`;
+  });
+});
